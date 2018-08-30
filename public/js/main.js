770d110004eaddaac007a37e7348671acfd85f92
milleriishaun <milleriishaun@gmail.com>
2018-08-30 16:32:22 -0700

    Edit main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,22 @@ $(document).ready(() => {
   // Place JavaScript code here...
   // TODO: FEAR NOT, rapid prototype using Prott and InVision
   // Created Prott low fidelity prototype
+  // NOW MAKE THE APP! ... it's just javascript which inclides the jQuery.
+  // Also, your .env.example assumes that you are already logged in to the system, and the APIs.
+  // The Javascript is thus, just jquery calls. and things like functions to change certain things on the page.
+  // This can also be react.
+  // jQuery information can be loaded to home pug template.
+  // Conclusion: Pug is a templating language, and not really
+  // used for React apps. The pug style uses old style programming
+  // paradigms. React is a library, but there is a whole
+  // framework involved that aims to solve the problems of complexity
+  // that arise from using things like Pug. There is however a babel-plugin-for-pug-to-react
+  // However, that plugin is only good on surface level.
+  // It makes something possible that is not necessarily a good practice.
+  // So, then, now I have to scrap this project, but I learned a lot.
+  // I will need to move to React, learn it, move into CRA+reactstrap, practice it, then move
+  // into react-boilerplate. Then I will have enough understanding to
+  // create the app that I intend on making.
   
 
 });
